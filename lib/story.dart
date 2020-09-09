30a60890704ffd2342168e830840093b829dfcd7
Adesoji Susan <42998872+adesojisusan@users.noreply.github.com>
2020-09-10 00:02:25 +0100

    Add files via upload

---
+++ lib/story.dart
@@ -0,0 +1,15 @@
+class Story{
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story (String a, String b, String c,){
+    storyTitle = a;
+    choice1 = b;
+    choice2 = c;
+  }
+}
+
+//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+
+//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
